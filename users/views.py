3090fa809bb51150ba65792a8eafab5f403859e6
sys <yuy8169@naver.com>
2023-04-15 12:48:51 +0900

    시작!

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
+from django.contrib.auth import logout as auth_logout
+from django.views.decorators.csrf import csrf_exempt
+import pymysql
+import platform
+
+
+# Create your views here.
+
+def mainpage (requset):
+    return render(requset,"users/mainpage.html")
