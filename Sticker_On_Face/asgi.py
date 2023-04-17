1b78e21abc7d60061b729330a22f0be93b50ac19
SYS <yuy8169@naver.com>
2023-04-17 14:17:21 +0900

    1111111111111

--- Sticker_On_Face/asgi.py
+++ Sticker_On_Face/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Sticker project.
+ASGI config for Sticker_On_Face project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sticker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sticker_On_Face.settings')
 
 application = get_asgi_application()
--- Sticker_On_Face/wsgi.py
+++ Sticker_On_Face/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Sticker project.
+WSGI config for Sticker_On_Face project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sticker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sticker_On_Face.settings')
 
 application = get_wsgi_application()
