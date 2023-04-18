45ddd94360e220e1ca410ebbe8d6a392911df8c7
sys <yuy8169@naver.com>
2023-04-19 03:32:37 +0900

    수정중

--- static/js/users/join.js
+++ static/js/users/join.js
@@ -1,37 +1,29 @@
 
-// 가입부분 체크
-
 function check_from(){
-
-  let name = document.getElementById("name").value
-  let ID = document.getElementById("user_ID").value
-  let password = document.getElementById("user_password").value
-  let Email = document.getElementById("mail").value
-   // 이메일확인
-
-  if(name == ''){
-  alert('이름을 입력해 주세요')
-  else{
-  alert('이름을 다시 입력해주세요')}}
-
-  if(ID == ''){
-  alert('아이디를 입력해 주세요')}
-  else{
-  alert('아이디를 다시 입력해주세요')}
-
-  if(password == ''){
-  alert('비밀번호를 입력해 주세요')}
-  else{
-  alert('비밀번호를 다시 입력해주세요')}
-  }
-  if(Email.includes('@')){
-    let emailId = email.split('@')[0]
-    let emailServer = email.split('@')[1]
-    if(emailId === "" || emailServer === ""){
-      document.getElementById("emailError").innerHTML="이메일이 올바르지 않습니다."
-      check = false
-
-
-
-
-
+	let name = document.getElementById("name").value
+	let ID = document.getElementById("user_ID").value
+	let password = document.getElementById("user_password").value
+    let phone = document.getElementById("phone").value
+	let Email = document.getElementById("mail").value
+	if(name == ''){
+		alert('이름을 입력해 주세요')
+	return false;
+	}
+    if(ID == ''){
+		alert('아이디를 입력해주세요');
+	return false;
+	}
+    if(password == ''){
+		alert('비밀번호를 입력해주세요');
+	return false;
+	}
+	if(phone == ''){
+		alert('전화번호를 입력해 주세요');
+	return false;
+	}
+	if(Email == ''){
+		alert('이메일을 입력해주세요');
+	return false;
+	}
+    return true;
+}
