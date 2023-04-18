95affa64dae7402941f348be8d0f2c7385811979
Kipyo <kipyobae1229@gmail.com>
2023-04-18 19:41:29 +0900

    user_views_find_myid update

--- users/views.py
+++ users/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 import pymysql
 import platform
 
+from mysql.connector import cursor
+
 
 # Create your views here.
 def login(request):
@@ -16,3 +18,42 @@ def signup(request):
 
 def find_id(request):
     return render(request, "users/find_id.html")
+
+def find_myid(request):
+    str_name = request.POST.get("MEMBER_NAME")
+    str_tel = request.POST.get("MEMBER_TEL")
+
+    filename="C:/tilburg_club/tilburg.txt"
+    with open(filename) as f:
+        root_ps=f.read()
+    print(root_ps)
+    print(filename)
+    dev_ps = root_ps+'dev'
+
+    conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='STICKER_ON_FACE', charset='utf8')
+    cur = conn.cursor()
+
+    sql_select = 'select MEMBER_ID from SOF_MEMBER where MEMBER_NAME = (%s) AND MEMBER_TEL = (%s)'
+    val = (str_name, str_tel)
+    cursor.execute(sql_select, val)
+
+    row = cur.fetchone()
+
+    if row is None:
+        return render(request, "user/find_id.html")
+    else:
+        str_id = row[0]
+        content = f'<h1>{str_id} is your id<h1>'
+    return HttpResponse(content)
+
+
+
+
+
+
+
+
+
+
+
+
