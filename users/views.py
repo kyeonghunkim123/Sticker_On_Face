4f164bd98d9ed5464a93eae57d4a3987c147f331
Kipyo <kipyobae1229@gmail.com>
2023-04-25 18:41:05 +0900

    commit for pull recent

--- users/views.py
+++ users/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.csrf import csrf_exempt
 import pymysql
 import platform
-
-from mysql.connector import cursor
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -13,64 +12,44 @@ def login(request):
     print('1')
     return render(request, "users/login.html")
 
+
 def signup(request):
     return render(request, "users/signup.html")
+
 
 def find_id(request):
     return render(request, "users/find_id.html")
 
-# 아이디 찾기
 def find_myid(request):
-    str_name = request.POST.get("MEMBER_NAME")
-    str_tel = request.POST.get("MEMBER_TEL")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        usertel = request.POST.get('usertel')
 
-    filename="C:/tilburg_club/tilburg.txt"
-    with open(filename) as f:
-        root_ps=f.read()
-    print(root_ps)
-    print(filename)
-    dev_ps = root_ps+'dev'
+        filename = "C:/tilburg_club/tilburg.txt"
+        with open(filename) as f:
+            root_ps = f.read().strip()
+        dev_ps = root_ps + 'dev'
 
-    conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='STICKER_ON_FACE', charset='utf8')
-    cur = conn.cursor()
+        conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='Sticker_On_Face',
+                               charset='utf8')
+        cur = conn.cursor()
+        sql = 'select MEMBER_ID from SOF_MEMBER where MEMBER_NAME = (%s) and MEMBER_TEL = (%s) '
+        val = (username, usertel)
 
-    sql_select = 'select MEMBER_ID from SOF_MEMBER where MEMBER_NAME = (%s) AND MEMBER_TEL = (%s)'
-    val = (str_name, str_tel)
-    cursor.execute(sql_select, val)
+        result =cur.execute(sql, val)
+        print(result)
 
-    row = cur.fetchone()
-
-    if row is None:
-        return render(request, "users/find_id.html")
-    else:
-        str_id = row[0]
-        content = f'<h1>{str_id} is your id<h1>'
-    return HttpResponse(content)
-
-
-
-
-
-
-
-
-
-
-
-
+        match = "True" if (result == "입력한 이름 전화번호") else "False"
+        # match = "True" if (result == None) else "False"
+        return JsonResponse({'success': f'{match}')
 
 def find_pw(request):
     return render(request, "users/find_pw.html")
 
-#비밀번호 찾기
-# id 만 하고 next 하는거라 str_tel은 아직 필요 없는 것 같음
 def find_mypw(request):
     str_id = request.POST.get("MEMBER_ID")
     # str_tel = request.POST.get("MEMBER_TEL")
-
-    print('===============================')
     print(str_id)   #, str_tel
-    print('===============================')
 
     filename="C:/tilburg_club/tilburg.txt"
     with open(filename) as f:
@@ -81,12 +60,10 @@ def find_mypw(request):
 
     conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='STICKER_ON_FACE', charset='utf8')
     cur = conn.cursor()
-    print('2')
     sql_select = 'select user_password from SOF_MEMBER where MEMBER_ID = (%s)'
     # and MEMBER_TEL = (%s)
     val = (str_id)  #, str_tel
-    a=cur.execute(sql_select, val)
-    print('3')
+    cur.execute(sql_select, val)
 
     row = cur.fetchone()
 
@@ -103,3 +80,85 @@ def find_mypw(request):
 
     print('5')
     return HttpResponse(content)
+
+def check_Id(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'POST 요청이 필요합니다.'}, status=400)
+    print(111111111)
+
+    userid = request.POST.get('userid')
+    print(211111111)
+
+    filename = "C:/tilburg_club/tilburg.txt"
+    with open(filename) as f:
+        root_ps = f.read().strip()
+    dev_ps = root_ps + 'dev'
+    conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='Sticker_On_Face', charset='utf8')
+    cur = conn.cursor()
+    sql = 'select MEMBER_ID from SOF_MEMBER where MEMBER_ID = (%s) '
+    val = (userid)
+    cur.execute(sql, val)
+
+    exists = "False" if (cur.fetchone() == None) else "True"
+    print("##  print(exists) : ", exists)
+    return JsonResponse({'exists': f"{exists}"})
+
+
+def check_login(request):
+    if request.method == 'POST':
+        userid = request.POST.get('userid')
+        userpw = request.POST.get('userpw')
+
+        print(111111111)
+
+        filename = "C:/tilburg_club/tilburg.txt"
+        with open(filename) as f:
+            root_ps = f.read().strip()
+        dev_ps = root_ps + 'dev'
+
+        conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='Sticker_On_Face',
+                               charset='utf8')
+
+        cur = conn.cursor()
+
+        sql = 'select MEMBER_ID from SOF_MEMBER where MEMBER_ID = (%s) and MEMBER_PW = (%s) '
+        val = (userid, userpw)
+
+        result =cur.execute(sql, val)
+        print(result)
+
+        exists = "False" if (result == None) else "True"
+        print("##  print(exists) : ", exists)
+        return JsonResponse({'success': "exists"})
+
+
+
+def check_join(request):
+    print('input')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        userid = request.POST.get('userid')
+        userpw = request.POST.get('userpw')
+        userphone = request.POST.get('userphone')
+        usermail = request.POST.get('usermail')
+
+        print('input1', username, userid, userpw, userphone, usermail)
+
+        filename = "C:/tilburg_club/tilburg.txt"
+        with open(filename) as f:
+            root_ps = f.read().strip()
+        dev_ps = root_ps + 'dev'
+        print('input2')
+        conn = pymysql.connect(host='130.162.154.239', user='dev', password=dev_ps, db='Sticker_On_Face',
+                               charset='utf8')
+        print('input3')
+        cur = conn.cursor()
+        print('input4')
+        sql_insert = 'insert into SOF_MEMBER (MEMBER_NAME, MEMBER_ID, MEMBER_PW, MEMBER_TEL,MEMBER_EMAIL) values(%s,%s,%s,%s,%s)'
+        val = (username, userid, userpw, userphone, usermail)
+        result = cur.execute(sql_insert, val)
+        exists = "False" if (result == None) else "True"
+        print("##  print(exists) : ", exists)
+        return JsonResponse({'success': f"{exists}"})
+
+    return JsonResponse({'error': 'Page not found.'}, status=404)
